fbe7c3a3c0aeca2cc8ef98ab4c43625f3eff2952
damian othacehe <damianothacehe@gmail.com>
2024-10-22 11:41:50 -0300

    componente checkout con formulario

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -1,9 +1,61 @@
-export default function Checkout(){
-    return(
+import { useForm } from "react-hook-form"
+import { useContext, useState } from "react";
+import { CartContext } from "../../context/cartContext";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+
+
+export default function Form(){
+
+    const [orderId, setOrderId] = useState("");
+    const { register, handleSubmit } = useForm();
+
+    const [cart] = useContext(CartContext);
+
+    const send = (data) => {
+        const order = {
+            buyer: data,
+            items: cart
+        }
+        console.log(order);
+
+        const orderRef = collection(db, "orders");
+        addDoc(orderRef, order).then((doc)=>{setOrderId(doc.id)});
+    }
+
+    if(orderId){
+        return(
+            <>
+            <h1> Gracias por tu compra</h1>
+            <p> Tu numero de orden es {orderId}</p>
+            </>
+        )
+    }
+
+
+    return (
         <>
-        <h1>
-            Checkout
-        </h1>
-        </>
+        <h1>Ingrese sus datos para finalizar la compra</h1>
+        <h3>Los campos marcados con un * son obligatorios</h3>
+        
+        <div className="formulario">
+            <form onSubmit={handleSubmit(send)}>
+
+                <label htmlFor="nombre">Ingresa tu nombre completo *</label>
+                <input type="text" id="nombre" placeholder="Nombre" required {...register("nombre")} />
+
+                <label htmlFor="telefono">Ingresa tu telefono</label>
+                <input type="number" id="telefono" placeholder="Telefono" {...register("telefono")} />
+
+                <label htmlFor="email">Ingresa tu e-mail *</label>
+                <input type="email" id="email" placeholder="e-mail" required />
+
+                <label htmlFor="email">Confirma tu e-mail *</label>
+                <input type="email" id="email" placeholder="e-mail" {...register("email")} />
+
+                <button type="submit">Finalizar compra</button>
+            </form>
+        </div>
+    </>
     )
 }
